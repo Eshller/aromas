2d89bf4206c7f33129b82fa351e81a8f02a15f6a
Eshu <eshu@Eshus-MacBook-Air.local>
2021-09-29 17:06:57 +0530

    Negative order fix

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-"http://192.168.0.108/get_items"
+function add(){
+    if(document.querySelector('#count').value < 0 ){
+        return 0;
+    }
+    document.querySelector('#count').value++;
+}
+function minus(){
+    if(document.querySelector('#count').value == 0){
+        return 0;
+    }
+    else{
+        document.querySelector('#count').value--;
+    }
+}
+
+// "http://192.168.0.108/get_items"
