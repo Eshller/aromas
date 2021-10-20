8c04a2f02f22d4704f814164e7b68d6fe42c989c
Aniket Agrawal <aniketagrawal@iitb.ac.in>
2021-10-20 16:54:17 +0530

    add

--- backend/index.js
+++ backend/index.js
@@ -9,23 +9,114 @@ server.listen(3000)
 app.use(express.json())
 app.use(cors());
 
-items = [["Coke", 0, 40, 1],
-        ["Chips", 0, 10, 1],
-        ["Sandwich", 0, 60, 0],
-        ["Paratha", 0, 36, 1],
-        ["Chicken Shourma", 1, 41, 1]]
+items = [
+    {
+        name: "Coke",
+        is_veg: true,
+        amt: 40,
+        is_avlbl: true,
+    },
+    {
+        name: "Chips",
+        is_veg: true,
+        amt: 10,
+        is_avlbl: true,
+    },
+    {
+        name: "Sandwich",
+        is_veg: true,
+        amt: 60,
+        is_avlbl: true,
+    },
+    {
+        name: "Paratha",
+        is_veg: true,
+        amt: 36,
+        is_avlbl: true,
+    },
+    {
+        name: "Chicken Shourma",
+        is_veg: true,
+        amt: 41,
+        is_avlbl: true,
+    },
+]
+
+pending_orders = [
+    // {
+    //     token: 0,
+    //     items: [
+    //         {
+    //             index: 0,
+    //             quantity: 2,
+    //         },
+    //         {
+    //             index: 1,
+    //             quantity: 3,
+    //         },
+    //     ],
+    //     total_amt: 110,
+    //     status: 0,
+    //     details: {
+    //         name: "Aniket",
+    //         phone: "9636541817",
+    //         hostel: "h3",
+    //     }
+    // },
+    // {
+    //     token: 1,
+    //     items: [
+    //         {
+    //             index: 0,
+    //             quantity: 2,
+    //         },
+    //         {
+    //             index: 1,
+    //             quantity: 3,
+    //         },
+    //     ],
+    //     total_amt: 110,
+    //     status: 0,
+    //     details: {
+    //         name: "Aniket",
+    //         phone: "9636541817",
+    //         hostel: "h3",
+    //     }
+    // },
+]
+
+curr_token = 0
 
 app.get("/get_items", async(req, res) => {
     res.status(201).send({items: items})
 })
 
-app.post("/take_order", async(req, res) => {
-    console.log(req.body)
-    res.status(201).send({token: 1})
+// app.post("/take_order", async(req, res) => {
+//     console.log(req.body)
+//     res.status(201).send({token: 1})
+// })
+
+app.post("/toggle_item", async(req, res) => {
+    data = req.body
+    console.log(data)
+    items[data.index].is_avlbl = data.toggle_value
+    console.log(items)
+    res.status(201).send({message: "success"})
 })
 
-app.post("/get_items", async(req, res) => {
-    res.status(201).send({confirm: 1})
+app.get("/get_pending_orders", async(req, res) => {
+    res.status(201).send({orders: pending_orders})
 })
 
+app.get('/', async(req, res)=>{
+    res.status(200).send("xxxxxxxx");
+})
 
+app.post('/take_order', async(req, res) => {
+    data = req.body
+    data.token = curr_token
+    curr_token += 1
+    data.status = 0
+    pending_orders.push(data)
+    res.status(201).send({token: data.token})
+})
